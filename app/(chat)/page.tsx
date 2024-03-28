092fcc6402dc4f30118b9c7e0fe26304267dc0a9
Jacksonmills <jacksonemills@gmail.com>
2024-03-27 20:10:06 -0500

    Add animation to chat component

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,7 +1,6 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
 import { AI } from '@/lib/chat/actions'
-import { Session } from '@/lib/types'
 import { getMissingKeys } from '../actions'
 
 export const metadata = {
